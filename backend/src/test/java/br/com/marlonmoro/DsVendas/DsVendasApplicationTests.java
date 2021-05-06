f4fa8c35ac91e137fe7acdcf2ee03a2d17dc4918
= <=>
2021-05-05 22:26:40 -0300

    Create projects

---
+++ backend/src/test/java/br/com/marlonmoro/DsVendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonmoro.DsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
